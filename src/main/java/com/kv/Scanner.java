fbc5161a5503f786c74c975036ffc91286f9dd05
dkkv0000 <dkkv0000@gmail.com>
2022-01-20 13:50:07 +0530

    Complete the scanner part

--- src/main/java/com/kv/Scanner.java
+++ src/main/java/com/kv/Scanner.java
@@ -1,7 +1,10 @@
 package com.kv;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import com.kv.TokenType.*;
 import static com.kv.TokenType.*;
 
@@ -9,6 +12,28 @@ import static com.kv.TokenType.*;
  * Scanner class is responsible for generating the tokens
  */
 public class Scanner {
+
+    private static final Map<String,TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
 
     private final String source;
     private final List<Token> tokenList = new ArrayList<>();
@@ -25,6 +50,8 @@ public class Scanner {
             start = current;
             scanToken();
         }
+
+        // TODO need to add end of the line code
         tokenList.add(null);
         return tokenList;
     }
@@ -54,20 +81,146 @@ public class Scanner {
                 if(match('/')){
                     /**
                      * as of now we are currently supporting the single line comment
-                     * we need to consume 
+                     * we need to consume until we see a new line char
+                     * or till we reach the end of the string
                      */
                     while(peek() != '\n' && !isAtEnd()) advance();
                 }
                 else {
                     addToken(SLASH);
                 }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+               // ignore the white space characters
+               break;
+            case '\n':
+               line++;
+               break;
+            case '"':
+                stringLiteral();
+                break;
             default:
-                Lox.error(line,"Unexpected Character");
+                if(isDigit(c)){
+                    numberLiteral();
+                }
+                /**
+                 * any lexeme that starting with a letter or underscore will
+                 * be treated as a identifier
+                 */
+                else if(isAlpha(c)){
+                    identifier();
+                }
+                else Lox.error(line,"Unexpected Character");
                 break;
         }
     }
 
+    private void identifier() {
+        while(isAlphaNumeric(peek())){
+            advance();
+        }
 
+        String text = source.substring(start,current);
+        TokenType type = keywords.get(text);
+        if(type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private boolean isAlpha(char c){
+        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_'));
+    }
+
+    private boolean isAlphaNumeric(char c){
+        return isDigit(c) || isAlpha(c);
+    }
+
+    private void numberLiteral() {
+
+        // consume all the digits
+        while(isDigit(peek())) advance();
+
+        // handle the fraction part
+        if(peek() == '.'){
+
+            // consume the '.' char
+            advance();
+
+            if(isDigit(peek())){
+
+                // consume the entire fraction part
+                while(isDigit(peek())) advance();
+            }
+            else {
+
+                // TODO custom
+                Lox.error(line,"trailing decimal point is not supported");
+                return;
+            }
+            // handle error
+        }
+
+        // we consumed all the digits that are part of string literal
+        addToken(NUMBER,Double.parseDouble(source.substring(start,current)));
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    /**
+     * This function is responsible for consuming the string literal
+     */
+    private void stringLiteral() {
+
+        // consume till we reach the end or till we find the '"' char
+        while(peek() != '"' && isAtEnd() == false){
+
+            // the string literal can be of more than two lines
+            // so if we also need to handle it
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        // handle unterminated String by reaching the end of string
+        if(isAtEnd()){
+            Lox.error(line,"Unterminated String literal");
+            return;
+        }
+
+        // consume the closing '"'
+        advance();
+
+        // get the string literal substring by stripping of the surrounding quotes
+        String literal = source.substring(start + 1,current - 1);
+
+        addToken(STRING,literal);
+    }
+
+    /**
+     * peek() will the return character located at the current pointer
+     * if all the characters are consumed then it returns a '\0'
+     *
+     * it does not advance to any next char
+     * @return
+     */
+    private char peek() {
+        // https://stackoverflow.com/questions/11294850/the-ascii-value-of-0-is-same-as-ascii-value-of-0
+        /** when we try to look for the next char and if the string is Empty at
+         *  that time we are returning null aka ('\0')
+         */
+        if(isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    /**
+     * this function checks whether the pointer to current char is same
+     * as the expected char
+     * if yes : advance to  next char
+     * @param expected char
+     * @return return true or false based on equality
+     */
     private boolean match(char expected) {
 
         // it is currently looking for next character
